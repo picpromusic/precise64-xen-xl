31d4ce9ff2acb6dad81ca34fa0a8a12bf0d0cbeb
Sebastian Sickelmann <sebastian.sickelmann@gmx.de>
2014-01-18 17:30:38 +0100

    apt-get update apt-get -y *

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #VBOX_VERSION=$(cat /etc/vagabond/vbox_version)
 VBOX_VERSION=$1
 
-apt-get install linux-headers-$(uname -r) linux-headers-generic build-essential xserver-xorg xserver-xorg-core
+apt-get -y update
+apt-get -y install linux-headers-$(uname -r) linux-headers-generic build-essential xserver-xorg xserver-xorg-core
 
 mkdir /mnt/iso
 wget http://download.virtualbox.org/virtualbox/$VBOX_VERSION/VBoxGuestAdditions_$VBOX_VERSION.iso
@@ -10,14 +11,14 @@ sh /mnt/iso/VBoxLinuxAdditions.run --nox11
 umount /mnt/iso
 rm VBoxGuestAdditions_$VBOX_VERSION.iso
 
-apt-get install xen-hypervisor-amd64
+apt-get -y install xen-hypervisor-amd64
 sed -i 's/GRUB_DEFAULT=.*\+/GRUB_DEFAULT="Ubuntu GNU/Linux, with Xen hypervisor"/' /etc/default/grub
 update-grub
 
 sed -i 's/TOOLSTACK=.*\+/TOOLSTACK="xl"/' /etc/default/xen
 
-apt-get remove linux-headers-$(uname -r) linux-headers-generic build-essential xserver-xorg xserver-xorg-core
-apt-get purge linux-headers-$(uname -r) linux-headers-generic build-essential xserver-xorg xserver-xorg-core
+apt-get -y remove linux-headers-$(uname -r) linux-headers-generic build-essential xserver-xorg xserver-xorg-core
+apt-get -y purge linux-headers-$(uname -r) linux-headers-generic build-essential xserver-xorg xserver-xorg-core
 unset VBOX_VERSION
 
 echo "Don't forgot to reboot"
